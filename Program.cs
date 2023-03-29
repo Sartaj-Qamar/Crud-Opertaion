edb65ae2bf6a19d2b0c6e758219c8a624a71578f
Sartaj Qamar <98673119+Sartaj-Qamar@users.noreply.github.com>
2023-03-29 21:16:03 +0500

    Add files via upload
    E-commerce website

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using E_Ticket.Data;
+using E_Ticket.Data.Services;
+using FluentAssertions.Common;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+//Services Configuration
+
+builder.Services.AddScoped<IActorsService, ActorsService>();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+// Seed database
+AppInitializer.Seed(app);
+
+
+app.Run();
